51da876299292dd0cd36f0897ae4ebd928e17e43
downinahill <brian.hill.0023@gmail.com>
2022-06-01 13:43:48 -0400

    fixed the tailwind issue

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const projects = [
+    {
+      title: "Sharing is Scary",
+      subtitle: "Full Stack Art Sharing App",
+      description:
+        "This app utilizes EJS and Multer to allow people to share their artwork with others.",
+      image: "Screen Shot 2022-02-06 at 4.24.15 PM.png",
+      link: "https://bestartshare.herokuapp.com/",
+    },
+    {
+      title: "Accompli",
+      subtitle: "MERN Stack Productivity App",
+      description: "Built with MongoDB, Express, React, and Node to provide a place where one can keep track of their goals.",
+      image: "Screen Shot 2022-02-05 at 12.51.04 PM.png",
+      link: "https://accompli-frontend-app.herokuapp.com/",
+    },
+    {
+      title: "Travel Advisor",
+      subtitle: "React app with APIs",
+      description: "A travel advisor app built using React, Google Maps API and Travel Advisor API.",
+      image: "Screen Shot 2022-05-16 at 12.46.31 PM.png",
+      link: "https://stupendous-pie-dc5cf7.netlify.app/",
+    },
+    {
+      title: "Budget Tracker",
+      subtitle: "React SPA",
+      description: "A budget tracker app built using React.",
+      image: "Screen Shot 2022-05-17 at 6.43.39 AM.png",
+      link: "https://friendly-basbousa-2c9315.netlify.app/",
+    },
+];
+
+
+export const skills = [
+
+    "CSS frameworks",
+    "JavaScript",
+    "React",
+    "Python",
+    "Node",
+    "Express",
+    "MongoDB"
+  
+  ];
+  
+  export const certifications = [
+    {
+      image: "https://i.imgur.com/0lREMdO.jpg"
+    }
+  ]
+  
+  export const badge = [
+    {
+      image: "https://imgur.com/a/ecag8J7"
+    }
+  ]
